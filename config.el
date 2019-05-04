5e800ad5f9583dab3de6fb46b008be9fb094d5c1
Zhe Li <linuxcity.jn@gmail.com>
2019-05-04 11:11:35 +0200

    Update README and author.

--- config.el
+++ config.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
 ;;
-;; Author: Zhe Li <zhe@zhe-mt-macbook-pro>
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
